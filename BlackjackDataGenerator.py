482ca6413ca2eae0e31265db9876ba3a320c1705
Robin Vonk <Robinvonk789@gmail.com>
2019-09-30 18:45:21 +0200

    More goodness
    Separated data refining logic to other file
    Only run the file if it runs as the main application
    Added new data to the refinement

--- BlackjackDataGenerator.py
+++ BlackjackDataGenerator.py
@@ -1,18 +1,18 @@
 from random import shuffle
 from random import randint
-from time import sleep
-
 import xlsxwriter
-import xlrd
 
 """CREDITS TO: https://brilliant.org/wiki/programming-blackjack/#"""
+"Generates a xlsx file with N number of simulated blackjack games"
+
+
 # Enum for readability
 DEALER = "Dealer"
 PLAYER = "Player"
 TIED = "Tied"
 
 WORKBOOK = 'BlackJackData.xlsx'  # Name of the file to read and write from
-DATATOCOLLECT = 10000  # The number of games to play
+DATATOCOLLECT = 1000  # The number of games to play
 ranks = [_ for _ in range(2, 11)] + ['JACK', 'QUEEN', 'KING', 'ACE']  # Values of the cards
 suits = ['SPADE', 'HEART ', 'DIAMOND', 'CLUB']  # Suits of the cards
 maxPlayerCards = 8  # Max cards for the Player, used for columns
@@ -20,23 +20,14 @@ maxDealerCards = 8  # Max cards for the Dealer, used for columns
 winnerColumn = 0  # The column in the sheet used to save the winner
 playerValueColumn = 1  # The column in the sheet used to save the value of the hand of the player
 dealerValueColumn = 2  # The column in the sheet used to save the value of the hand of the dealer
-playerColumnPointer = 3  # Current column to write the player card to
-dealerColumnPointer = maxPlayerCards + 3  # Current column to write the dealer card to
-currentLine = 1  # Current row to write to
-playing = 1  # Status of the player
 
-workbook = xlsxwriter.Workbook(WORKBOOK)
-worksheet = workbook.add_worksheet()
 
-# Add the first row column names tot the sheet
-worksheet.write(0, winnerColumn, "Winner")
-worksheet.write(0, playerValueColumn, "Player value")
-worksheet.write(0, dealerValueColumn, "Dealer value")
-for card in range(maxPlayerCards):
-    worksheet.write(0, 3 + card, "Player" + str(card))
-
-for card in range(maxDealerCards):
-    worksheet.write(0, 3 + maxPlayerCards + card, "Dealer" + str(card))
+def getCardFromString(string):
+    string = str(string).replace(']', '').replace('[', '').replace('\'', '')
+    string = string.split(',')
+    if str.isdigit(string[0]):
+        string[0] = int(string[0])
+    return string
 
 
 # Get a deck of cards as an array
@@ -74,159 +65,110 @@ def hand_value(hand):
     return tmp_value
 
 
-# Run the test
-for test in range(DATATOCOLLECT):
-    # get a deck of cards and shuffle it
-    deck = get_deck()
-    shuffle(deck)
+if __name__ == "__main__":
+    playerColumnPointer = 3  # Current column to write the player card to
+    dealerColumnPointer = maxPlayerCards + 3  # Current column to write the dealer card to
+    currentLine = 1  # Current row to write to
+    playing = 1  # Status of the player
 
-    # Draw the starting hand
-    player_hand = [deck.pop(), deck.pop()]
-    dealer_hand = [deck.pop()]
+    workbook = xlsxwriter.Workbook(WORKBOOK)
+    worksheet = workbook.add_worksheet()
 
-    # Save starting hand
-    worksheet.write(currentLine, playerColumnPointer, str(player_hand[0]))
-    playerColumnPointer += 1
-    worksheet.write(currentLine, playerColumnPointer, str(player_hand[1]))
-    playerColumnPointer += 1
-    worksheet.write(currentLine, dealerColumnPointer, str(dealer_hand[0]))
-    dealerColumnPointer += 1
+    # Add the first row column names tot the sheet
+    worksheet.write(0, winnerColumn, "Winner")
+    worksheet.write(0, playerValueColumn, "Player value")
+    worksheet.write(0, dealerValueColumn, "Dealer value")
+    for card in range(maxPlayerCards):
+        worksheet.write(0, 3 + card, "Player" + str(card))
 
-    # This loops only ends when a winner or tie is found
-    while playing:
-        valuePlayer = hand_value(player_hand)
-        if valuePlayer == 21:
-            worksheet.write(currentLine, playerValueColumn, valuePlayer)
-            worksheet.write(currentLine, dealerValueColumn, card_value(dealer_hand[0]))  # Dealer has not played yet
-            worksheet.write(currentLine, winnerColumn, PLAYER)
-            break
-        elif valuePlayer >= 21:
-            worksheet.write(currentLine, playerValueColumn, valuePlayer)
-            worksheet.write(currentLine, dealerValueColumn, card_value(dealer_hand[0]))  # Dealer has not played yet
-            worksheet.write(currentLine, winnerColumn, DEALER)
-            break
-        else:
-            # There are 2 options: draw or pass, player will draw till at least a value of 12 and then draw at random
-            if valuePlayer <= 11:
-                player_hand.append(deck.pop())
-                worksheet.write(currentLine, playerColumnPointer, str(player_hand[len(player_hand) - 1]))
-                playerColumnPointer += 1
-                continue
+    for card in range(maxDealerCards):
+        worksheet.write(0, 3 + maxPlayerCards + card, "Dealer" + str(card))
 
-            choice = randint(0, 2)
-            # Choice one is draw card
-            if choice == 1:
-                player_hand.append(deck.pop())
-                worksheet.write(currentLine, playerColumnPointer, str(player_hand[len(player_hand) - 1]))
-                playerColumnPointer += 1
-                continue
+    # Run the test
+    for test in range(DATATOCOLLECT):
+        # get a deck of cards and shuffle it
+        deck = get_deck()
+        shuffle(deck)
 
-            # Choice two is pass
-            if choice == 0:
-                # Dealer draws to at least a value of 17
-                while hand_value(dealer_hand) < 17:
-                    dealer_hand.append(deck.pop())
-                    worksheet.write(currentLine, dealerColumnPointer, str(dealer_hand[len(dealer_hand) - 1]))
-                    dealerColumnPointer += 1
+        # Draw the starting hand
+        player_hand = [deck.pop(), deck.pop()]
+        dealer_hand = [deck.pop()]
 
-                # Compare the value of both hands and determine the result
-                valueDealer = hand_value(dealer_hand)
-                if valueDealer >= 21:
-                    worksheet.write(currentLine, playerValueColumn, valuePlayer)
-                    worksheet.write(currentLine, dealerValueColumn, valueDealer)
-                    worksheet.write(currentLine, winnerColumn, PLAYER)
-                    break
+        # Save starting hand
+        worksheet.write(currentLine, playerColumnPointer, str(player_hand[0]))
+        playerColumnPointer += 1
+        worksheet.write(currentLine, playerColumnPointer, str(player_hand[1]))
+        playerColumnPointer += 1
+        worksheet.write(currentLine, dealerColumnPointer, str(dealer_hand[0]))
+        dealerColumnPointer += 1
 
-                if valueDealer < valuePlayer:
-                    worksheet.write(currentLine, playerValueColumn, valuePlayer)
-                    worksheet.write(currentLine, dealerValueColumn, valueDealer)
-                    worksheet.write(currentLine, winnerColumn, PLAYER)
-                    break
-
-                if valueDealer > valuePlayer:
-                    worksheet.write(currentLine, playerValueColumn, valuePlayer)
-                    worksheet.write(currentLine, dealerValueColumn, valueDealer)
-                    worksheet.write(currentLine, winnerColumn, DEALER)
-                    break
-
-                if valueDealer == valuePlayer:
-                    worksheet.write(currentLine, playerValueColumn, valuePlayer)
-                    worksheet.write(currentLine, dealerValueColumn, valueDealer)
-                    worksheet.write(currentLine, winnerColumn, TIED)
-                    break
+        # This loops only ends when a winner or tie is found
+        while playing:
+            valuePlayer = hand_value(player_hand)
+            if valuePlayer == 21:
+                worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                worksheet.write(currentLine, dealerValueColumn, card_value(dealer_hand[0]))  # Dealer has not played yet
+                worksheet.write(currentLine, winnerColumn, PLAYER)
                 break
-    # Reset the variables for the next game
-    currentLine += 1
-    dealerColumnPointer = maxPlayerCards + 3
-    playerColumnPointer = 3
+            elif valuePlayer >= 21:
+                worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                worksheet.write(currentLine, dealerValueColumn, card_value(dealer_hand[0]))  # Dealer has not played yet
+                worksheet.write(currentLine, winnerColumn, DEALER)
+                break
+            else:
+                # There are 2 options: draw or pass, player will draw till at least a value of 12 and then draw at random
+                if valuePlayer <= 11:
+                    player_hand.append(deck.pop())
+                    worksheet.write(currentLine, playerColumnPointer, str(player_hand[len(player_hand) - 1]))
+                    playerColumnPointer += 1
+                    continue
 
-""" DO EXPERIMENT THINGS HERE"""
-"This cannot be done in another file, because once the excelsheet(workbook) is closed, " \
-"xlsxwriter cannot edit it anymore"
+                choice = randint(0, 2)
+                # Choice one is draw card
+                if choice == 1:
+                    player_hand.append(deck.pop())
+                    worksheet.write(currentLine, playerColumnPointer, str(player_hand[len(player_hand) - 1]))
+                    playerColumnPointer += 1
+                    continue
 
-# Reading an excel file using Python
-TotalRows = 0  # Count the number of games in the sheet
-PlayerWins = 0  # Count the number of games won by the player
-DealerWins = 0  # Count the number of games won by the dealer
-TieGames = 0  # Count the number of games tied
-Errors = 0  # Count the number of invalid rows
-reader = xlrd.open_workbook(WORKBOOK)  # Create a reader to read the xlsx file
-readsheet = reader.sheet_by_index(0)  # Select the sheet with the data
-chartsheet = workbook.add_worksheet()  # Add new sheet for the graphs
+                # Choice two is pass
+                if choice == 0:
+                    # Dealer draws to at least a value of 17
+                    while hand_value(dealer_hand) < 17:
+                        dealer_hand.append(deck.pop())
+                        worksheet.write(currentLine, dealerColumnPointer, str(dealer_hand[len(dealer_hand) - 1]))
+                        dealerColumnPointer += 1
 
-# Loop to refine the data
-for row in range(readsheet.nrows):
-    # The first row contains column names
-    if row == 0:
-        continue
-    for column in range(maxPlayerCards):
-        tempValue = str(readsheet.cell_value(row, column + 1))
-        print(tempValue)
-    tempValue = str(readsheet.cell_value(row, winnerColumn))
-    TotalRows += 1
-    # Sum how many wins the play and the dealer have
-    if tempValue == DEALER:
-        DealerWins += 1
-    elif tempValue == PLAYER:
-        PlayerWins += 1
-    elif tempValue == TIED:
-        TieGames += 1
-    else:
-        Errors += 1
+                    # Compare the value of both hands and determine the result
+                    valueDealer = hand_value(dealer_hand)
+                    if valueDealer >= 21:
+                        worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                        worksheet.write(currentLine, dealerValueColumn, valueDealer)
+                        worksheet.write(currentLine, winnerColumn, PLAYER)
+                        break
 
-print("Total games: " + str(TotalRows) + " Dealer Wins: " + str(DealerWins) + " Player Wins: " + str(
-    PlayerWins) + " Tie Games: " + str(TieGames) + " Errors: " + str(Errors))
+                    if valueDealer < valuePlayer:
+                        worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                        worksheet.write(currentLine, dealerValueColumn, valueDealer)
+                        worksheet.write(currentLine, winnerColumn, PLAYER)
+                        break
 
-# Write refined data to the sheet for the graphs
-chartsheet.write(0, 0, DEALER)
-chartsheet.write(0, 1, PLAYER)
-chartsheet.write(0, 2, TIED)
+                    if valueDealer > valuePlayer:
+                        worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                        worksheet.write(currentLine, dealerValueColumn, valueDealer)
+                        worksheet.write(currentLine, winnerColumn, DEALER)
+                        break
 
-chartsheet.write(1, 0, DealerWins)
-chartsheet.write(1, 1, PlayerWins)
-chartsheet.write(1, 2, TieGames)
+                    if valueDealer == valuePlayer:
+                        worksheet.write(currentLine, playerValueColumn, valuePlayer)
+                        worksheet.write(currentLine, dealerValueColumn, valueDealer)
+                        worksheet.write(currentLine, winnerColumn, TIED)
+                        break
+                    break
+        # Reset the variables for the next game
+        currentLine += 1
+        dealerColumnPointer = maxPlayerCards + 3
+        playerColumnPointer = 3
 
-# Create a new column chart  to display the number of wins
-chart = workbook.add_chart({'type': 'column'})
-
-# Add the configuration to the chart
-chart.add_series({'values': ['Sheet2', 1, 0, 1, 2],
-                  'name': 'Wins',
-                  'categories': ['Sheet2', 0, 0, 0, 2],
-                  'fill': {'color': 'blue'}})
-
-chart.set_title({
-    'name': 'Number of wins',
-})
-chart.set_x_axis({
-    'name': 'Possible outcome'
-})
-chart.set_y_axis({
-    'name': 'Number of wins'
-})
-
-# Insert the chart into the worksheet.
-chartsheet.insert_chart(2, 2, chart)
-
-# Cleanup
-workbook.close()
+    # Cleanup
+    workbook.close()
---
+++ BlackjackDataRefiner.py
@@ -0,0 +1,145 @@
+import xlsxwriter
+import xlrd
+import BlackjackDataGenerator as bdg
+
+""" DO EXPERIMENT THINGS HERE"""
+"This cannot add the charts to the existing xlsx file, so it creates it own"
+
+DATASOURCE = bdg.WORKBOOK  # Name of the file to read from
+WORKBOOK = 'charts.xlsx'  # Name of the file to write the graphs to
+DIFFICULTHAND = [14, 15, 16]
+
+if __name__ == "__main__":
+    # Reading an excel file using Python
+    TotalRows = 0  # Count the number of games in the sheet
+    PlayerWins = 0  # Count the number of games won by the player
+    DealerWins = 0  # Count the number of games won by the dealer
+    TieGames = 0  # Count the number of games tied
+    Errors = 0  # Count the number of invalid rows
+    reader = xlrd.open_workbook(DATASOURCE)  # Create a reader to read the xlsx file
+    readsheet = reader.sheet_by_index(0)  # Select the sheet with the data
+    workbook = xlsxwriter.Workbook(WORKBOOK)
+    writesheet = workbook.add_worksheet()  # Add new sheet for the graphs
+
+    difficultGames = 0  # Number of difficult cases
+    difficultWins = 0  # Player wins difficult
+    difficultLoses = 0  # Player wins difficult
+    difficultTies = 0  # Player wins difficult
+    difficultWinsDraw = 0  # Player wins by drawing in difficult case
+    difficultWinsPass = 0
+    difficultLosesDraw = 0  # Player loses by passing in difficult case
+    difficultLosesPass = 0
+
+    # Loop to refine the data
+    for row in range(readsheet.nrows):
+        # The first row contains column names
+        if row == 0:
+            continue
+
+        tempValue = str(readsheet.cell_value(row, bdg.winnerColumn))
+        TotalRows += 1
+        # Sum how many wins the play and the dealer have
+        if tempValue == bdg.DEALER:
+            DealerWins += 1
+        elif tempValue == bdg.PLAYER:
+            PlayerWins += 1
+        elif tempValue == bdg.TIED:
+            TieGames += 1
+        else:
+            Errors += 1
+
+        starterHand = []
+        starterHandValue = 0
+        # Loop over the player hand
+        for column in range(bdg.maxPlayerCards):
+            tempValue = readsheet.cell_value(row, 3 + column)
+            # extract the starting hand from the sheet
+            if column <= 1:
+                starterHand.append(bdg.getCardFromString(tempValue))
+                starterHandValue = bdg.hand_value(starterHand)
+                # Extract difficult starterhand
+                if column == 1 and starterHandValue in DIFFICULTHAND:
+                    difficultGames += 1
+                    # Check if the player won
+                    winner = readsheet.cell_value(row, bdg.winnerColumn)
+                    if winner == bdg.PLAYER:
+                        difficultWins += 1
+                        # If then player won check if he drew or passed
+                        card3 = bdg.getCardFromString(readsheet.cell_value(row, 6))
+                        if card3[0] == '':  # if passed
+                            difficultWinsPass += 1
+                        else:
+                            difficultWinsDraw += 1
+                    elif winner == bdg.DEALER:
+                        difficultLoses += 1
+                        # If then player lost check if he drew or passed
+                        card3 = bdg.getCardFromString(readsheet.cell_value(row, 6))
+                        if card3[0] == '':  # if passed
+                            difficultLosesPass += 1
+                        else:
+                            difficultLosesDraw += 1
+                    else:
+                        difficultTies += 1
+
+    print("Total: " + str(TotalRows) + " Total games: " + str(difficultGames) + " Dealer Wins: " + str(difficultLoses) + " Player Wins: " + str(
+        difficultWins) + " Tie Games: " + str(difficultTies) + " winpass: " + str(difficultWinsPass) + " windraw: " + str(
+        difficultWinsDraw))
+
+    # Write refined data to the sheet for the graphs
+    columnpointer1 = 0
+    writesheet.write(0, columnpointer1, bdg.DEALER)
+    writesheet.write(1, columnpointer1, DealerWins)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, bdg.PLAYER)
+    writesheet.write(1, columnpointer1, PlayerWins)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, bdg.TIED)
+    writesheet.write(1, columnpointer1, TieGames)
+    columnpointer1 += 2
+    writesheet.write(0, columnpointer1, 'Difficult')
+    writesheet.write(1, columnpointer1, difficultGames)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DWin')
+    writesheet.write(1, columnpointer1, difficultWins)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DLose')
+    writesheet.write(1, columnpointer1, difficultLoses)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DTied')
+    writesheet.write(1, columnpointer1, difficultTies)
+    columnpointer1 += 2
+    writesheet.write(0, columnpointer1, 'DWinPass')
+    writesheet.write(1, columnpointer1, difficultWinsPass)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DWinDraw')
+    writesheet.write(1, columnpointer1, difficultWinsDraw)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DLosePass')
+    writesheet.write(1, columnpointer1, difficultLosesPass)
+    columnpointer1 += 1
+    writesheet.write(0, columnpointer1, 'DLoseDraw')
+    writesheet.write(1, columnpointer1, difficultLosesDraw)
+
+    # Create a new column chart  to display the number of wins
+    chart = workbook.add_chart({'type': 'column'})
+
+    # Add the configuration to the chart
+    chart.add_series({'values': ['Sheet1', 1, 0, 1, 2],
+                      'name': 'Wins',
+                      'categories': ['Sheet1', 0, 0, 0, 2],
+                      'fill': {'color': 'blue'}})
+
+    chart.set_title({
+        'name': 'Number of wins'
+    })
+    chart.set_x_axis({
+        'name': 'Possible outcome'
+    })
+    chart.set_y_axis({
+        'name': 'Number of wins'
+    })
+
+    # Insert the chart into the worksheet.
+    writesheet.insert_chart(4, 2, chart)
+
+    workbook.close()
